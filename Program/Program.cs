09db14432e3289ea5022557e348c9d9071bfd636
Selim_eh58 <Selim_eh58@STHQ0127-02>
2023-01-13 10:58:13 +0400

    Add project files.

---
+++ Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using PetShop;
+
+Cat c = new("Graf", 5, "Male", 12, 50, 20);
+Cat c1 = new("Prince", 7, "Female", 10, 40, 30);
+
+c.Play();
+c.Sleep();
+c.Eat();
+
+c1.Play();
+c1.Sleep();
+
+CatHouse house = new();
+house.AddCat(c);
+house.AddCat(c1);
+
+house.ShowCats();
